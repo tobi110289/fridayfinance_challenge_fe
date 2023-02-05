39bcdd89c6701ba1270434b90a42ba2844ae9d50
tobi110289 <tobias.feltes@web.de>
2023-02-05 01:11:58 +0100

    feat(global): setup pages

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+const message = ref('Hello')
+const change = () => {
+  message.value = 'Goodbye'
+}</script>
+
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <button v-on:click="change">Goodbye</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ pages/transactions.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Page: transactions
+    <h1>TRANSACTIONS</h1>
+  </div>
+</template>
+
+<style scoped></style>
